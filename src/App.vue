5045b6876ab4d31187b486301eec83765a05d8db
pappagnu <andreas@klonad.se>
2017-02-24 16:23:15 +0100

    Added 'AddTodo' component to app

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,15 @@
 import Lists from './components/Lists'
 import Test from './components/Test'
 import Rock from './components/Rock'
+import AddTodo from './components/AddTodo'
 
 export default {
   name: 'app',
   components: {
     Lists,
     Test,
-    Rock
+    Rock,
+    AddTodo
   }
 }
 </script>
